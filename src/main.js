93f942f72376921358499d18d6d3118836592d3f
Julián David Reales de la Ossa <103285208+jrealesdelaossa@users.noreply.github.com>
2023-03-18 19:02:45 +0000

    app terminada primer borrador

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// importación de Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./main.css"
+
 createApp(App).mount('#app')
